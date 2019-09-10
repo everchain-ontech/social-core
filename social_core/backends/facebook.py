8bb35de0205374346e882e07ebc4bea6f505a534
Mr-Ye <shiguodong@everchain.info>
2019-09-10 13:06:32 +0800

    add id key for facebook backend

--- social_core/backends/facebook.py
+++ social_core/backends/facebook.py
@@ -168,6 +168,7 @@ class FacebookOAuth2(BaseOAuth2):
 class FacebookAppOAuth2(FacebookOAuth2):
     """Facebook Application Authentication support"""
     name = 'facebook-app'
+    ID_KEY = 'id'
 
     def uses_redirect(self):
         return False
